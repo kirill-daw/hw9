99cff129f5fe1b09eb9f7f83959c8185d617c49b
Кирилл <kirill-daw2002@yandex.ru>
2025-04-20 00:25:28 +0500

    Homework's done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task 1: ");
+        int expensesOfMonth[] = {23000, 45000, 29000, 34000, 60000};
+        int sum = 0;
+        for (int i : expensesOfMonth) {
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        System.out.println("Task 2: ");
+        int expensesOfMonth1[] = {43000, 29000, 32000, 22000, 34000};
+        int maxExpensesOfWeek = 0;
+        for (int i : expensesOfMonth1) {
+            if (i >= maxExpensesOfWeek) {
+                maxExpensesOfWeek = i;
+            }
+        }
+        int minExpensesOfWeek = maxExpensesOfWeek;
+        for (int i : expensesOfMonth1) {
+            if (minExpensesOfWeek >= i) {
+                minExpensesOfWeek = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minExpensesOfWeek + " рублей.");
+        System.out.println("Максимальная сумма трат за неделю составила " + maxExpensesOfWeek + " рублей.");
+
+        System.out.println("Task 3: ");
+        int expensesOfMonth2[] = {33000, 28000, 22000, 43000, 34000};
+        sum = 0;
+        for (int i : expensesOfMonth2) {
+            sum += i;
+        }
+        float averageExpensesOfWeek = (float) sum / expensesOfMonth2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageExpensesOfWeek + " рублей.");
+
+        System.out.println("Task 4: ");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
